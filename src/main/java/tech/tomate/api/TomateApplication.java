6f7d70db39584f8974b0dd884cea26ab380de2d0
Ronaldo e Silva Vieira <ronaldo.vieira@dcc.ufmg.br>
2022-09-11 18:35:46 -0300

    Add Spring Boot project starter files

---
+++ src/main/java/tech/tomate/api/TomateApplication.java
@@ -0,0 +1,13 @@
+package tech.tomate.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/tomate/api/TomateApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.tomate.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
